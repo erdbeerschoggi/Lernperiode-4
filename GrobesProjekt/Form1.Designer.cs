8c7582be12deba15214a861f4605e0a5add78a27
erdbeerschoggi <M.Piotrowska.inf24@stud.bbbaden.ch>
2025-02-14 10:57:33 +0100

    Add files via upload

---
+++ GrobesProjekt/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace GrobesProjekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(203, 31);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Was Möchtest du Essen?";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(62, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 235);
+            button1.TabIndex = 1;
+            button1.Text = "Auswährts essen";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(445, 147);
+            button2.Name = "button2";
+            button2.Size = new Size(250, 235);
+            button2.TabIndex = 2;
+            button2.Text = "Zuhause essen";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(21, 67);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(203, 31);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "Wähle eine Option";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox2;
+    }
+}
